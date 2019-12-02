86422d2830015b12011a57a1da5605c41a2ad242
Pau Martín Arnau <paumartinsecre@gmail.com>
2019-12-02 09:58:11 +0100

    SAMBA:BASE mode detach

--- samba19:base/startup.sh
+++ samba19:base/startup.sh
@@ -3,7 +3,18 @@
 # startup.sh
 # -------------------------------------
 
+# Afegim els usuaris UNIX i SAMBA
+useradd lila
+useradd roc
+useradd patipla
+useradd pla
+echo -e "lila\nlila" | smbpasswd -a lila
+echo -e "roc\nroc" | smbpasswd -a roc
+echo -e "patipla\npatipla" | smbpasswd -a patipla
+echo -e "pla\npla" | smbpasswd -a pla
+
+
+# Iniciem els processos tot
 /opt/docker/install.sh && echo "Install Ok"
-/usr/sbin/smbd && echo "smb Ok"
 /usr/sbin/nmbd && echo "nmb  Ok"
-/bin/bash
+/usr/sbin/smbd -F && echo "smb Ok"
